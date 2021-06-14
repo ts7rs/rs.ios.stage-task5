99fb1ff9cf358006af7e225d13a0ef5a73d82c34
ts7rs <ts-7@yandex.ru>
2021-06-14 21:25:29 +0200

    feat: add implementation for subtasks 1,2 of task 5

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -2,7 +2,25 @@ import Foundation
 
 class StockMaximize {
 
-    func countProfit(prices: [Int]) -> Int {
-        return -1
-    }
+  func countProfit(prices: [Int]) -> Int {
+    let newArray = prices
+    var profit = 0
+    let maxV = newArray.max()
+    let indexOfMax = newArray.lastIndex(of: maxV ?? 0)
+    let arraySlice = newArray.prefix(indexOfMax ?? 0)
+    let newArray1 = Array(arraySlice)
+        
+      for value in newArray1 {
+        profit += (maxV ?? 0) - value
+      }
+      if (newArray.count - arraySlice.count) > 1 {
+        let arraySlice1 = newArray.suffix(from: (indexOfMax ?? 0) + 1)
+        let newArray2 = Array(arraySlice1)
+        let maxV = newArray2.max()
+          for value in newArray2 {
+            profit += (maxV ?? 0) - value
+          }
+      }
+        return profit
+  }
 }
--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -2,7 +2,69 @@ import UIKit
 
 class MessageDecryptor: NSObject {
     
-    func decryptMessage(_ message: String) -> String {
-        return ""
+  func decryptMessage(_ message: String) -> String {
+    let stringIncoming = message
+    var stringResult = ""
+    var stringAdd = ""
+    var stringNumber = ""
+    var num = 0
+    let arraySplit1 = stringIncoming.components(separatedBy: "]")
+        
+    func matches(for regex: String, in text: String) -> [String] {
+      do {
+        let regex = try NSRegularExpression(pattern: regex)
+        let results = regex.matches(in: text, range: NSRange(text.startIndex..., in: text))
+      return results.map {
+        String(text[Range($0.range, in: text)!])
+      }
+      } catch let error {
+        print("invalid regex: \(error.localizedDescription)")
+        return []
+        }
     }
+        
+    func checkStringNumber(str: String, str2: String) -> String   {
+      var stringWithoutDigit = (str.components(separatedBy: CharacterSet.decimalDigits)).joined(separator: "")
+      let part = str.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
+        if !part.isEmpty {
+          let numInner = Int(part) ?? 1
+          stringWithoutDigit.append(String(repeating: str2, count: numInner))
+        }
+        return stringWithoutDigit
+    }
+        
+    for element in arraySplit1 {
+    let arraySplit2 = element.components(separatedBy: "[")
+        
+    if arraySplit2.count == 1 {
+      stringResult.append(arraySplit2[0])
+    }
+    if arraySplit2.count > 1 {
+      stringNumber = arraySplit2[0]
+      num = Int(stringNumber) ?? 1
+
+      if num == 1{
+        let matchedNumber = matches(for: "[0-9]", in: stringNumber)
+        let stringSliceWithoutNumber = matchedNumber[0 ..< matchedNumber.endIndex]
+        let stringNum = stringSliceWithoutNumber.joined(separator: "")
+        let matchedString = matches(for: "[a-z]", in: stringNumber)
+        let stringSliceWithoutNumber1 = matchedString[0 ..< matchedString.endIndex]
+        let string = stringSliceWithoutNumber1.joined(separator: "")
+        num = Int(stringNum) ?? 1
+        stringResult.append(string)
+      }
+        
+     if arraySplit2.count > 2 {
+       let stringTest = checkStringNumber(str: arraySplit2[1], str2: arraySplit2[2])
+       stringAdd = String(repeating: stringTest, count: num)
+       stringResult.append(stringAdd)
+     }
+    else {
+      stringAdd = String(repeating: arraySplit2[1], count: num)
+      stringResult.append(stringAdd)
+    }
+   }
+  }
+return stringResult
+ }
 }
